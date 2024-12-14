161be83cf40ec641435b82a9e4d4f69a0d062e35
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-12-14 14:57:06 +0530

    payment model is created

---
+++ server/models/Payment.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose "
+
+const paymentSchema = new Schema({
+
+    paymentMode: {
+        type: String,
+        required: true
+
+    },
+    amount: {
+        type: Number,
+        required: true
+
+    },
+    transactionId: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        defualt: "pending "
+    },
+
+},
+    {
+        timestamp: true
+    });
+
+const Payment = model("Payment", paymentSchema);
+
+export default Payment;
