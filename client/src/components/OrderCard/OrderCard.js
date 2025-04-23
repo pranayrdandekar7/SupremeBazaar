c06fa94c965a0d5833d53e21f62718323967a29e
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2025-04-23 20:03:30 +0530

    order card implememnted

---
+++ client/src/components/OrderCard/OrderCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ReadableTImeStamp = (date) => {
+    const dateobj = new Date(date)
+    return `${dateobj.getDate()}/${dateobj.getMonth() + 1}/${dateobj.getFullYear()} 
+    ${dateobj.getHours()}:${dateobj.getMinutes()} 
+    ${dateobj.getHours() >= 12 ? "PM" : "AM"} `
+}
+
+
+
+
+const OrderCard = ({ order }) => {
+
+    const { _id, status, products, createdAt, totalBill, deliveryAddress } = order
+    console.log(order)
+    return (
+        <div className='m-4 p-4 bg-white rounded-lg relative'>
+            <p>Order ID:{_id} , Order On : {ReadableTImeStamp(createdAt)}</p>
+
+            <p className='text-lg font-bold mt-3'>
+                {products.map((product) => product.productId.name).join(",")}
+            </p>
+            <p> Total Amount : {totalBill} ₹</p>
+            <p>Address : {deliveryAddress}</p>
+            <span className='bg-blue-500 rounded-full px-3 py-1 text-white absolute top-2 right-2 text-sm'>{status}</span>
+        </div>
+
+    )
+}
+
+export default OrderCard;
--- client/src/views/UserOrders/UserOrders.js
+++ client/src/views/UserOrders/UserOrders.js
@@ -3,6 +3,7 @@ import { getCurrentUser, getJwtToken } from "../../utils/common"
 import React ,{useState ,useEffect}from 'react'
 import toast ,{Toaster} from "react-hot-toast"
 import axios from "axios" ;
+import OrderCard from "../../components/OrderCard/OrderCard";
 
 const UserOrders = () => {
 
@@ -24,10 +25,8 @@ const UserOrders = () => {
                 }
             }
         ) ;
-     
 
         setOrders(response.data.data)
-        console.log(response.data.data)
 
         }
         catch(error){
@@ -62,6 +61,18 @@ const UserOrders = () => {
     <div>
         <h1>UserOrders </h1>
         <p>current user : {user.name} </p>
+
+        <div>
+            {
+                orders.map((order)=>{
+
+                    return <OrderCard  key={order._id} order={order}/>
+
+                })
+            }
+
+        </div>
+       
         <Toaster/>
 
       
