9c06527f1c021254165bf9cd4bf703a755ddf825
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2025-04-28 03:39:50 +0530

    dialouge box created for order details

--- client/src/views/UserOrders/UserOrders.js
+++ client/src/views/UserOrders/UserOrders.js
@@ -1,85 +1,95 @@
 
 import { getCurrentUser, getJwtToken } from "../../utils/common"
-import React ,{useState ,useEffect}from 'react'
-import toast ,{Toaster} from "react-hot-toast"
-import axios from "axios" ;
+import React, { useState, useEffect } from 'react'
+import toast, { Toaster } from "react-hot-toast"
+import axios from "axios";
 import OrderCard from "../../components/OrderCard/OrderCard";
 
 const UserOrders = () => {
 
 
-    const [user ,setUser] = useState ({})
-    const [orders ,setOrders] = useState ([]) 
+    const [user, setUser] = useState({})
+    const [orders, setOrders] = useState([])
 
 
-    const loadUserOrders = async ()=>{
-        if(!user?._id){
-            return ;
+    const loadUserOrders = async () => {
+        if (!user?._id) {
+            return;
         }
-       
-        try{
-        const response =  await axios.get(`${process.env.REACT_APP_API_URL}/orders/user/${user._id}`,
-            {
-                headers:{
-                    Authorization:getJwtToken() 
+
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/orders/user/${user._id}`,
+                {
+                    headers: {
+                        Authorization: getJwtToken()
+                    }
                 }
-            }
-        ) ;
+            );
 
-        setOrders(response.data.data)
+            setOrders(response.data.data)
 
         }
-        catch(error){
-            
+        catch (error) {
+
             toast.error(error.response.data.message)
         }
 
 
     }
 
-    useEffect (()=>{
-        const user = getCurrentUser() 
-        if (user){
-            setUser (user)
+    useEffect(() => {
+        const user = getCurrentUser()
+        if (user) {
+            setUser(user)
         }
         else {
             toast.error("Plsease Login To Access This Page")
-            setTimeout(()=>{
+            setTimeout(() => {
                 window.location.href = "/login"
             }, 2000
-        )
+            )
         }
-    }, [] )
+    }, [])
 
-    useEffect(()=>{
-        loadUserOrders() ;
-    } ,[user])
-     
+    useEffect(() => {
+        loadUserOrders();
+    }, [user])
 
 
-  return (
-    <div>
-        <h1>UserOrders </h1>
-        <p>current user : {user.name} </p>
+    const OrderviewDialogue = ({ isOpen ,OnClose }) => {
+        return (
+            <div className="min-h-screen bg-gray-700 fixed left-0 top-0 w-full bg-opacity-75 z-50 flex justify-center items-center"> 
+                <div className="bg-white w-1/2 h-96  rounded-lg relative"> 
+                <button className="text-red-500 font-bold  absolute right-2 top-1">Close</button>
+                
+                </div>
 
+            </div>
+        )
+    }
+
+    return (
         <div>
-            {
-                orders.map((order)=>{
+            <h1>UserOrders </h1>
+            <p>current user : {user.name} </p>
 
-                    return <OrderCard  key={order._id} order={order}/>
+            <div>
+                {
+                    orders.map((order) => {
 
-                })
-            }
+                        return <OrderCard key={order._id} order={order} />
 
+                    })
+                }
+
+            </div>
+            <OrderviewDialogue />
+
+            <Toaster />
         </div>
-       
-        <Toaster/>
 
-      
-        </div>
-    
-   
-  )
+
+    )
 }
 
-export default UserOrders ;
+export default UserOrders;
