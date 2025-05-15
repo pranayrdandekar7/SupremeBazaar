c4662809bdce5df2f41be2f6eb0ed2c432fd9e57
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2025-05-15 18:04:24 +0530

    logic for change image on arrow

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react' 
-import { ChevronLeft as LeftArrowIcon , ChevronRight as RightArrowIcon } from "lucide-react"
+import React, { useState } from 'react'
+import { ChevronLeft as LeftArrowIcon, ChevronRight as RightArrowIcon } from "lucide-react"
 
 const ProductCard = ({
     name,
@@ -12,16 +12,28 @@ const ProductCard = ({
     images }) => {
 
     const [currentImage, setCurrentImage] = useState(images[0]);
+
+    const leftArrowClick = () => {
+        const currentIndex = images.indexOf(currentImage);
+        const newIndex = currentIndex > 0 ? currentIndex - 1 : images.length - 1;
+        setCurrentImage(images[newIndex]);
+    }
+
+    const rightArrowClick = () => {
+        const currentIndex = images.indexOf(currentImage);
+        const newIndex = currentIndex < images.length - 1 ? currentIndex + 1 : 0;
+        setCurrentImage(images[newIndex])
+    }
     return (
 
         <div className='bg-white shadow-lg rounded-lg m-5 px-5 py-2 overflow-hidden max-w-[300px] '>
 
             <div className='relative bg-gray-200'>
-                <LeftArrowIcon size={36} className='absolute left-0.5 top-1/2' />
+                <LeftArrowIcon size={36} className='absolute left-0.5 top-1/3 cursor-pointer' onClick={leftArrowClick} />
                 <img src={currentImage}
                     alt={name}
                     className='object-contain w-full h-40 rounded-lg ' />
-                <RightArrowIcon size={36} className='absolute right-0.5 top-1/2' />
+                <RightArrowIcon size={36} className='absolute right-0.5 top-1/3 cursor-pointer' onClick={rightArrowClick} />
 
             </div>
             <h1 className='font-bold text-lg'>{name.length > 15
