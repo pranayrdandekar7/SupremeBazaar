0e67a1a463b5b3db039b47f2713d3ea6fe044913
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2025-05-20 01:30:16 +0530

    add to cart functonality created

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -6,8 +6,10 @@ import {
     Minus as MinusIcon
 } from "lucide-react"
 import Button from '../Button/Button';
+import toast,{Toaster} from 'react-hot-toast';
 
 const ProductCard = ({
+    _id,
     name,
     price,
     currentPrice,
@@ -18,7 +20,7 @@ const ProductCard = ({
     images }) => {
 
     const [currentImage, setCurrentImage] = useState(images[0]);
-    const [quantity ,setQuantity] = useState(1)
+    const [quantity, setQuantity] = useState(1)
 
     const leftArrowClick = () => {
         const currentIndex = images.indexOf(currentImage);
@@ -31,7 +33,40 @@ const ProductCard = ({
         const newIndex = currentIndex < images.length - 1 ? currentIndex + 1 : 0;
         setCurrentImage(images[newIndex])
     }
-    
+
+    const handelAddToCard = () => {
+        const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+
+        const product = {
+            productId: _id,
+            name: name,
+            image: currentImage,
+            quantity: quantity,
+            price: currentPrice
+        }
+
+        let existingProductIndex = -1;
+
+        cart.forEach((item, index) => {
+
+            if (item.productId === _id) {
+                existingProductIndex = index;
+            }
+        })
+        if (existingProductIndex > -1) {
+            cart[existingProductIndex].quantity = quantity;
+        }
+        else {
+            cart.push(product);
+        }
+
+
+
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        toast.success("Product added to cart");
+    }
+
     return (
 
         <div className='bg-white shadow-lg rounded-lg m-5 px-3 py-2 overflow-hidden w-[300px] h-[370px] relative '>
@@ -68,23 +103,25 @@ const ProductCard = ({
                 <del> ₹ {price}</del> <span className='font-bold'>{currentPrice} /-</span>
             </p>
             <div className='flex w-[100px] items-center  justify-around  mx-auto mt-1'>
-                <MinusIcon size={26} className=" cursor-pointer" onClick={()=>{
+                <MinusIcon size={24} className=" cursor-pointer" onClick={() => {
                     setQuantity(quantity - 1)
                 }} />
-                <span className='text-2xl'> {quantity}</span>
-                <PlusIcon size={26} className=" cursor-pointer" onClick={()=>{
-                    setQuantity( quantity + 1)
+                <span className='text-xl'> {quantity}</span>
+                <PlusIcon size={24} className=" cursor-pointer" onClick={() => {
+                    setQuantity(quantity + 1)
                 }} />
 
 
             </div>
 
-            <div className=" absolute bottom-3 left-1/3">
+            <div className=" absolute bottom-3 left-1/3 ">
                 <Button
                     label="Add To Cart"
                     variant="primary"
+                    onClick={handelAddToCard}
                 />
             </div>
+            <Toaster/>
 
         </div>
 
